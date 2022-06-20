9468a215eba1c57ab68d63e03c7a6c296044fdf5
Koecha <koecha@localhost.localdomain>
2022-06-20 03:08:52 +0300

    procfile

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ "origin": 'https://realtors-homes.netlify.app/'});
+  app.enableCors({ "origin": '*' });
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
