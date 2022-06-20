0ba774e7b759d0edebd3f2277a6b03deb61841c5
Koecha <koecha@localhost.localdomain>
2022-06-20 16:21:36 +0300

    body parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ "origin": '*' });
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
