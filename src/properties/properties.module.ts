1b32e7f2b086073e2805d872b5c676b6a2d9f1f5
Peter571 <peterkoech1624@gmail.com>
2022-06-24 03:24:07 +0300

    guards

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -3,10 +3,11 @@ import { PropertiesService } from './properties.service';
 import { PropertiesController } from './properties.controller';
 import { PropertySchema } from './properties.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }])],
-  controllers: [PropertiesController],
+  controllers: [PropertiesController, JwtStrategy],
   providers: [PropertiesService]
 })
 export class PropertiesModule {}
