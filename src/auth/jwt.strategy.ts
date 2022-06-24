9a8b6994184eb43039797b8f7050f5077a1cef88
Peter571 <peterkoech1624@gmail.com>
2022-06-24 03:14:12 +0300

    guards

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Payload } from 'src/types';
+import { ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+import { AuthService } from './auth.service';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(private authService: AuthService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
@@ -12,5 +16,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         })
     }
 
+    async validate(payload: Payload, done: VerifiedCallback): Promise<any> {
+        const user = await this.authService.validateUser(payload);
+        if (!user) {
+          return done(
+            new HttpException('Unauthorized access', HttpStatus.UNAUTHORIZED),
+            false,
+          );
+        }
+        return done(null, user);
+      }
 
 }
