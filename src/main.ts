68bf71f3df255812e179607a9b614f546a15fa1a
Peter571 <peterkoech1624@gmail.com>
2022-06-25 04:35:16 +0300

    url update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ "origin": '*' });
+  app.enableCors({ "origin": 'https://realtors-homes.netlify.app' });
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(process.env.PORT || 8080);
